1f125e1c52e2cfb6acebcbe1303e670a4072c1a0
Eitan Evnin <eitaneeeee@gmail.com>
2025-05-26 22:44:01 +0300

    github actions fixing 4

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -7,30 +7,14 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr_block
 }
 
-resource "aws_subnet" "my_subnet" {
-  cidr_block = var.subnet_cidr_block
-  vpc_id     = aws_vpc.my_vpc.id
-  availability_zone = "us-east-2"
-}
 
-resource "aws_security_group" "my_security_group" {
-  name        = "my-security-group"
-  description = "Security group for my ELB"
-  vpc_id      = aws_vpc.my_vpc.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+module "github_actions" {
+  source = "./modules/github_actions"
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  repository = "my-repo"
+  aws_access_key_id_secret_name = "AWS_ACCESS_KEY_ID_LOCALSTACK"
+  aws_secret_access_key_secret_name = "AWS_SECRET_ACCESS_KEY_LOCALSTACK"
 }
 
 module "elb" {
--- infrastructure/terraform/modules/elb/main.tf
+++ infrastructure/terraform/modules/elb/main.tf
@@ -1,3 +1,29 @@
+resource "aws_subnet" "my_subnet" {
+  cidr_block = var.subnet_cidr_block
+  vpc_id     = aws_vpc.my_vpc.id
+  availability_zone = "us-east-2"
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "my-security-group"
+  description = "Security group for my ELB"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_elb" "my_elb" {
   name            = "my-elb"
   subnets         = [aws_subnet.my_subnet.id]
