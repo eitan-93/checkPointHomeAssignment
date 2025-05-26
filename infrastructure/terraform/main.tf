c76fbd87fd79f5f14ffe3e5049022c2521dba800
Eitan Evnin <eitaneeeee@gmail.com>
2025-05-26 18:55:34 +0300

    modules reference issue 4

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -34,14 +34,14 @@ resource "aws_security_group" "my_security_group" {
 }
 
 module "elb" {
-  source = "./elb.tf"
+  source = "./"
 
   subnets         = [aws_subnet.my_subnet.id]
   security_groups = [aws_security_group.my_security_group.id]
 }
 
 module "ecs" {
-  source = "./ecs.tf"
+  source = "./"
 
   cluster_name = "my-cluster"
   instance_type = "t2.micro"
