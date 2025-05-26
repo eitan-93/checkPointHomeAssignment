e0b5b82e563bc42e638f1900bafe06f92bcb2ad7
Eitan Evnin <eitaneeeee@gmail.com>
2025-05-26 22:46:35 +0300

    github actions fixing 5

--- infrastructure/terraform/modules/github_actions/main.tf
+++ infrastructure/terraform/modules/github_actions/main.tf
@@ -52,15 +52,15 @@ resource "aws_iam_role_policy_attachment" "github_actions" {
 }
 
 # Create GitHub Actions secrets
-resource "github_actions_secret" "aws_access_key_id" {
-  repository = var.repository
-  secret_name = var.aws_access_key_id_secret_name
-}
+#resource "github_actions_secret" "aws_access_key_id" {
+#  repository = var.repository
+#  secret_name = var.aws_access_key_id_secret_name
+#}
 
-resource "github_actions_secret" "aws_secret_access_key" {
-  repository = var.repository
-  secret_name = var.aws_secret_access_key_secret_name
-}
+#resource "github_actions_secret" "aws_secret_access_key" {
+#  repository = var.repository
+#  secret_name = var.aws_secret_access_key_secret_name
+#}
 
 resource "github_repository_file" "example-workflow" {
   repository = var.repository
@@ -70,13 +70,13 @@ resource "github_repository_file" "example-workflow" {
 
 # Create a GitHub Actions workflow that uses the AWS CLI with the assumed IAM role
 resource "github_actions_secret" "aws_access_key_id" {
-  repository = "my-repo"
+  repository = "checkPointHomeAssignment"
   secret_name = "AWS_ACCESS_KEY_ID_LOCALSTACK"
   value = "my-access-key-id"
 }
 
 resource "github_actions_secret" "aws_secret_access_key" {
-  repository = "my-repo"
+  repository = "checkPointHomeAssignment"
   secret_name = "AWS_SECRET_ACCESS_KEY_LOCALSTACK"
   value = "my-secret-access-key"
 }
