3d6ca362757b4806dd788fde0dff04dd8dfe943d
Eitan Evnin <eitaneeeee@gmail.com>
2025-05-26 22:50:48 +0300

    github actions fixing 6

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -7,6 +7,32 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr_block
 }
 
+resource "aws_subnet" "my_subnet" {
+  cidr_block = var.subnet_cidr_block
+  vpc_id     = aws_vpc.my_vpc.id
+  availability_zone = "us-east-2"
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "my-security-group"
+  description = "Security group for my ELB"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 
 module "github_actions" {
@@ -51,4 +77,12 @@ module "sqs" {
   source = "./modules/sqs"
 
   queue_name = "my-queue"
+}
+
+output "subnet_id" {
+  value = aws_subnet.my_subnet.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.my_security_group.id
 }
--- infrastructure/terraform/modules/elb/main.tf
+++ infrastructure/terraform/modules/elb/main.tf
@@ -1,33 +1,7 @@
-resource "aws_subnet" "my_subnet" {
-  cidr_block = var.subnet_cidr_block
-  vpc_id     = aws_vpc.my_vpc.id
-  availability_zone = "us-east-2"
-}
-
-resource "aws_security_group" "my_security_group" {
-  name        = "my-security-group"
-  description = "Security group for my ELB"
-  vpc_id      = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_elb" "my_elb" {
   name            = "my-elb"
-  subnets         = [aws_subnet.my_subnet.id]
-  security_groups = [aws_security_group.my_security_group.id]
+  subnets         = [var.subnet_id]
+  security_groups = [var.security_group_id]
 
   listener {
     instance_port      = 5000
@@ -35,4 +9,9 @@ resource "aws_elb" "my_elb" {
     lb_port            = 80
     lb_protocol        = "http"
   }
+}
+
+resource "aws_elb" "my_elb" {
+  subnets         = [var.subnet_id]
+  security_groups = [var.security_group_id]
 }
