7a586771330a75003b104f6e94b5feccfaf5f85e
Eitan Evnin <eitaneeeee@gmail.com>
2025-05-27 10:02:12 +0300

    checking aws provider 8

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -13,7 +13,7 @@ terraform {
 
 provider "aws" {
   alias = "main"
-  region = var.region
+  region = "local"
   #FOR TESTING
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
