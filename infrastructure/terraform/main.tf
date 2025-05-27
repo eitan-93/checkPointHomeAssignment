8a57d1ac82a2641397074a77910dbc7a2693fd7c
Eitan Evnin <eitaneeeee@gmail.com>
2025-05-27 15:52:38 +0300

    testing aws directly

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -11,34 +11,38 @@ terraform {
   }
 }
 
-
 provider "aws" {
-  region                      = "us-east-2"
-  #FOR TESTING
-  access_key = "test"
-  secret_key = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_use_path_style           = true
-  endpoints {
-    s3             = "http://localhost:4566"
-    ec2            = "http://localhost:4566"
-    iam            = "http://localhost:4566"
-    sts            = "http://localhost:4566"
-    elb            = "http://localhost:4566"
-    ssm            = "http://localhost:4566"
-    sqs            = "http://localhost:4566"
-    ecs            = "http://localhost:4566"
-    cloudwatch     = "http://localhost:4566"
-    autoscaling    = "http://localhost:4566"
-  }
-
-  shared_credentials_files = ["/dev/null"]
-
-  token                   = "dummy"
-  
+  region = "us-east-2"
 }
+
+#local test
+#provider "aws" {
+#  region                      = "us-east-2"
+#  #FOR TESTING
+#  access_key = "test"
+#  secret_key = "test"
+#  skip_credentials_validation = true
+#  skip_metadata_api_check     = true
+#  skip_requesting_account_id  = true
+#  s3_use_path_style           = true
+#  endpoints {
+#    s3             = "http://localhost:4566"
+#    ec2            = "http://localhost:4566"
+#    iam            = "http://localhost:4566"
+#    sts            = "http://localhost:4566"
+#    elb            = "http://localhost:4566"
+#    ssm            = "http://localhost:4566"
+#    sqs            = "http://localhost:4566"
+#    ecs            = "http://localhost:4566"
+#    cloudwatch     = "http://localhost:4566"
+#    autoscaling    = "http://localhost:4566"
+#  }
+#
+#  shared_credentials_files = ["/dev/null"]
+#
+#  token                   = "dummy"
+#  
+#}
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr_block
