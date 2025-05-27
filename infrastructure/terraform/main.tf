f9a1df7a8bc4fd5bb6e9140b78fc273d948bb3df
Eitan Evnin <eitaneeeee@gmail.com>
2025-05-27 08:34:23 +0300

    changed location of github provider due to warning

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "4.5.0"
+    }
+  }
+}
+
+
 provider "aws" {
   alias = "main"
   region = var.region
