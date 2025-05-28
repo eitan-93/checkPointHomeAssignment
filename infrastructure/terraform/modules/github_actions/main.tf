9e465086f6f9a2c8bab6fc9b69f9e2e03c0797dc
Eitan Evnin <eitaneeeee@gmail.com>
2025-05-28 12:12:27 +0300

    testing workflow on aws - upload 25

--- infrastructure/terraform/modules/github_actions/main.tf
+++ infrastructure/terraform/modules/github_actions/main.tf
@@ -1,15 +1,3 @@
-resource "github_actions_secret" "aws_access_key_id" {
-  repository = var.repository
-  secret_name = var.aws_access_key_id_secret_name
-  plaintext_value = "my-access-key-id"
-}
-
-resource "github_actions_secret" "aws_secret_access_key" {
-  repository = var.repository
-  secret_name = var.aws_secret_access_key_secret_name
-  plaintext_value = "my-secret-access-key"
-}
-
 
 resource "github_actions_secret" "aws_access_key_id" {
   repository      = var.repository
